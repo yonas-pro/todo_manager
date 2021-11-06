8dbe011f4b63c99834d1cb7a28cd56d9c0069dac
Yonas Alemayehu <itsyonaspro@gmail.com>
2021-11-06 08:22:49 +0300

    build an appbar with toast message

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
-import 'package:todo_manager/constants/app_color.dart' as color;
+import 'package:intl/intl.dart';
+import 'package:todo_manager/constants/app_color.dart' as my_color;
 import 'package:todo_manager/services/theme_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,23 +13,98 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final toast = FToast();
+
+  @override
+  void initState() {
+    super.initState();
+    toast.init(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: context.theme.backgroundColor,
-        leading: IconButton(
-          onPressed: () {
-            ThemeService().changeTheme();
-          },
-          icon: Icon(
-            Icons.lightbulb,
-            color: Get.isDarkMode ? color.white : color.black,
-          ),
-        ),
-      ),
+      appBar: _buildAppBar(),
       body: const Center(
         child: Text('Testing'),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: context.theme.backgroundColor,
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 10),
+          Text(
+            'Today',
+            style: TextStyle(
+                color: Get.isDarkMode ? my_color.white : my_color.black),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            DateFormat.yMMMMd().format(DateTime.now()),
+            style: TextStyle(
+              color: Get.isDarkMode
+                  ? Colors.grey[500]
+                  : Colors.grey.withOpacity(0.8),
+              fontSize: 13,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        IconButton(
+          onPressed: _changeTheme,
+          icon: Icon(
+            Get.isDarkMode ? Icons.lightbulb_outline : Icons.lightbulb,
+            color: my_color.blue,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _changeTheme() {
+    ThemeService().changeTheme();
+    _showBottomToast(
+      'Theme changed!',
+      Icons.home,
+      my_color.blue.withOpacity(0.2),
+    );
+  }
+
+  void _showBottomToast(String message, IconData icon, Color color) {
+    toast.showToast(
+      child: _buildToast(message, icon, color),
+      gravity: ToastGravity.BOTTOM,
+      fadeDuration: 400,
+      toastDuration: const Duration(milliseconds: 1200),
+    );
+  }
+
+  Container _buildToast(String message, IconData icon, Color color) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, color: Get.isDarkMode ? my_color.black : my_color.white),
+          const SizedBox(width: 10),
+          Text(
+            message,
+            style: TextStyle(
+              color: Get.isDarkMode ? my_color.black : my_color.white,
+            ),
+          ),
+        ],
       ),
     );
   }
